ab00d036f6d9406836d9fb0f8abacdb0db6ce699
Alex Zvaniga <azvaniga@carlton.ca>
2018-12-07 12:06:52 -0500

    added div with hello

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,21 +76,21 @@
     </head>
     <body>
 
-        <div class="flex-center position-ref full-height background">
-            @if (Route::has('login'))
-                Nav
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <div name="title" class="flex-center position-ref full-height background">
+            <!-- @if (Route::has('login')) -->
+                <!-- Nav Bar-->
+                <!-- <div class="top-right links"> -->
+                    <!-- @auth -->
+                        <!-- <a href="{{ url('/home') }}">Home</a> -->
+                    <!-- @else -->
+                        <!-- <a href="{{ route('login') }}">Login</a> -->
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                        <!-- @if (Route::has('register')) -->
+                            <!-- <a href="{{ route('register') }}">Register</a> -->
+                        <!-- @endif -->
+                    <!-- @endauth -->
+                <!-- </div> -->
+            <!-- @endif -->
 
             <div class="content">
                 <div class="title m-b-md">
@@ -101,14 +101,13 @@
                     <a href="">Travel Photography</a>
                     <a href="">In My Shoes...</a>
                     <a href="">About Me</a>
-                    <!-- <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a> -->
                 </div>
             </div>
+        </div>
+        <div name="shoes" class="flex-center position-ref full-height">
+            <h3>
+                Hello
+            </h3>
         </div>
     </body>
 </html>
